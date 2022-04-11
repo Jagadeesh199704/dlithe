74889fc5477769ffec7f968892fa66d8b47f86d1
Jagadeesh <jaggu5733@gmail.com>
2022-04-12 00:42:21 +0530

    initialcommit

---
+++ Front-end/register-form/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  cities = ['Bangalore', 'Hydrabad', 'Mangalore','Koppal'];
+
+  userModel = new User('', '','','')
+}
